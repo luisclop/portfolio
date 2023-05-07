3002d44e86e3444ad3fff42ae6251e843ae23324
Luis Lopez <lclopez220@gmail.com>
2023-05-07 13:32:42 -0500

    Actualiza basename del router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root, Home, About, Portfolio, Contact } from "./routes";
 
 export default function App() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Root />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     {
-      path: "/",
-      element: <Root />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-  ]);
+      // This is the default value for the "basename" prop
+      basename: "/portfolio",
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
